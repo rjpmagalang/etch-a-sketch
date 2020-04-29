339a5fedb19af526367f93c96f89f5ae4425cc1c
rjpmagalang <42713764+rjpmagalang@users.noreply.github.com>
2020-04-29 23:30:14 +0800

    reupload: uploaded wrong files

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+const container = document.querySelector("#container");
+const input = document.querySelector("#input");
+const reset = document.querySelector("#reset");
+const black = document.querySelector("#black");
+const shade = document.querySelector("#shade");
+const random = document.querySelector("#random");
 
-function inputLength() {
-  return input.value.length;
-}
+input.addEventListener("keypress", addInputNum);
 
-function createListElement() {
-  var li = document.createElement("li");
-  li.addEventListener("click", toggleClassItem);
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
-
-  var remove = document.createElement("button");
-  remove.innerHTML = "remove";
-  remove.addEventListener("click", removeItem);
-  li.appendChild(remove);
-
-  function removeItem() {
-    var item = this.closest("li");
-    item.remove();
-  };
-
-  function toggleClassItem() {
-    var item = this.closest("li");
-    li.classList.toggle("done");
+function addInputNum() {
+    if (event.keyCode === 13) {  
+        container.textContent = ""; 
+        createGrid();
+    }
   }
+
+function createGrid() {
+    let gridNum = document.querySelector("#gridNum");
+    
+    let grid = input.value;
+
+    if (isNaN(grid)){
+        alert("not a number");
+        location.reload();
+    } else if (grid > 100) {
+        alert("the number you entered is greater than 100");
+        location.reload();
+    }
+
+    for(i=0; i<grid*grid; i++){
+        let createDiv = document.createElement("div");
+        container.appendChild(createDiv);
+    }
+
+    input.value === "" ? gridNum.innerText = "" : gridNum.innerText = grid + " x " + grid;
+
+    container.style.setProperty("--colNum", grid);
+    container.style.setProperty("--rowNum", grid); 
 }
 
-function addListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  }
+reset.addEventListener("click", clearGrid);
+
+function clearGrid() {
+    let gridItems = document.querySelectorAll('div');
+    gridItems.forEach(div => {
+        div.style.backgroundColor = "aquamarine";
+    });
 }
 
-function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) {
-    createListElement();
-  }
+black.addEventListener("click", blackColor);
+
+function blackColor() {
+    container.addEventListener("mouseover", () => {
+        event.target.style.backgroundColor = "black";
+    });
 }
 
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeypress);
+random.addEventListener("click", randomColor);
+
+function randomColor() {
+    container.addEventListener("mouseover", () => {
+        const r = Math.floor(Math.random()*255);
+        const g = Math.floor(Math.random()*255);
+        const b = Math.floor(Math.random()*255);
+        const rgb = "rgb(" + r + ", " + g+ ", " + b + ")";
+        event.target.style.backgroundColor = rgb;
+    });
+}
+
+shade.addEventListener("click", shadeColor);
+
+function shadeColor() {
+    let num = 0;
+    num = num + 0.1;
+
+    container.addEventListener("mouseover", () => {
+        event.target.style.backgroundColor = "rgba(0, 0, 0, " + (num++ * 0.1) + ")";     
+    })
+}
